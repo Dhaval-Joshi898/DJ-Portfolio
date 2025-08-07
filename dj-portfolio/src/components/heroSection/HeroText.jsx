f1add2bc84f85963ad795f1c2a98a0b65cb788d7
Dhaval <joshidhaval2002@gmail.com>
2025-08-07 23:09:08 +0530

    main hero section of th eportfolio created

---
+++ dj-portfolio/src/components/heroSection/HeroText.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className='flex flex-col gap-4 h-full justify-center md:text-left sm:text-center'>
+        <h2 className='lg:text-2xl sm:text-xl uppercase text-cyan'>Aspiring full stack developer</h2>
+        <h1 className='md:text-[2.8rem] lg:text-6xl sm:text-4xl font-bold font-special text-orange'>Dhaval Joshi</h1>
+        <p className='text-lg mt-4 text-white'>
+            A passionate developer aspiring to become <br></br>full-stack developer.
+        </p>
+    </div>
+  )
+}
+
+export default HeroText
