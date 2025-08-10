dd5fa5233122a0c390ae3a9176d9a8be21d2be74
Dhaval <joshidhaval2002@gmail.com>
2025-08-10 18:54:36 +0530

    added footer navbar,skills,about me,contact me section components

--- dj-portfolio/src/App.jsx
+++ dj-portfolio/src/App.jsx
@@ -1,10 +1,29 @@
-import "./App.css";
+import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
+import ContactMeMain from "./components/contactMeSection/ContactMeMain";
+import FooterMain from "./components/footer/FooterMain";
+import HelperSection from "./components/HelperSection";
+import HeroGradient from "./components/heroSection/HeroGradient";
+import HeroMain from "./components/heroSection/HeroMain";
+import SubHeroSection from "./components/heroSection/SubHeroSection";
 import NavbarMain from "./components/navbar/NavbarMain";
+import ProjectMain from "./components/projectSection/ProjectMain";
+import SkillsBaseImg from "./components/skillSection/SkillsBaseImg";
+import SkillsMain from "./components/skillSection/SkillsMain";
 
 function App() {
   return (
     <main className="font-body">
       <NavbarMain />
+      <HeroMain />
+      <HeroGradient />
+      <SubHeroSection />
+      <AboutMeMain />
+      <SkillsMain/>
+      <SkillsBaseImg/>
+      <ProjectMain/>
+      <ContactMeMain/>
+      <FooterMain/>
+      {/* <HelperSection/> */}
     </main>
   );
 }
