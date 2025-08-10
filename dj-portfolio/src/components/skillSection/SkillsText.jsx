425f5ae198ad1007ac4fbfef7d1b8f6191b0c9b0
Dhaval <joshidhaval2002@gmail.com>
2025-08-10 18:55:06 +0530

    skills text

---
+++ dj-portfolio/src/components/skillSection/SkillsText.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SkillsText = () => {
+  return (
+    <div className="flex flex-col items-center mt-[100px]">
+      <h2 className="text-6xl text-cyan mb-10">My Skills</h2>
+      <p className="text-lg text-center text-white">
+        I have worked with these tech stack below, and have acquired hands on
+        experience by making projects <br></br> using these technologies.
+      </p>
+    </div>
+  );
+};
+
+export default SkillsText;
