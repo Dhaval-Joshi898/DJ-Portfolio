e29d77afcf0eead930051475a64a3cdbb3104af2
Dhaval <joshidhaval2002@gmail.com>
2025-08-10 17:08:24 +0530

    Contact Me section done

---
+++ dj-portfolio/src/components/contactMeSection/ContactInfo.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HiOutlineMail } from "react-icons/hi";
+import { FiPhone } from "react-icons/fi";
+import { IoLocationOutline } from "react-icons/io5";
+import SingleInfo from './SingleInfo';
+const ContactInfo = () => {
+  return (
+    <div className='flex flex-col gap-4 text-white'>
+      <SingleInfo text="joshidhaval2002@gmail.com" Image={HiOutlineMail}/>
+      <SingleInfo text="8097062771" Image={FiPhone}/>
+      <SingleInfo text="mumbai" Image={IoLocationOutline}/>
+    </div>
+  )
+}
+
+export default ContactInfo
---
+++ dj-portfolio/src/components/contactMeSection/ContactSocial.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SingleContactSocial from './SingleContactSocial'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+
+const ContactSocial = () => {
+  return (
+    <div className='flex gap-4'>
+        <SingleContactSocial link="#" Icon={FaGithub}/>
+        <SingleContactSocial link="#" Icon={FaLinkedin}/>
+        <SingleContactSocial link="#" Icon={SiLeetcode}/>
+
+    </div>
+  )
+}
+
+export default ContactSocial
